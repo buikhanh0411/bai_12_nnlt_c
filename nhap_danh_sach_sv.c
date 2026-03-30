61f62e8d65dcc5b982cd5517567eef69d752b859
khanh <khanhdacodau2007@gmail.com>
2026-03-30 15:32:45 +0700

    feat: update nhap danh sach

--- nhap_danh_sach_sv.c
+++ nhap_danh_sach_sv.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
-void nhap_danh_sach_sv(struct SinhVien danh_sach_lop[],int *p_so_luong_sv){
-    printf("nhap so luong sinh vien: ");
-    scanf("%i",p_so_luong_sv);
-    for (int i = 0; i < *p_so_luong_sv; i++){
-        printf("nhap ma sinh vien: ");
-        scanf("%s",danh_sach_lop[i].ma_sinhvien);
-        printf("nhap ho dem sinh vien: ");
-        scanf("%s",danh_sach_lop[i].hodem_sinhvien);
-        printf("nhap ten sinh vien: ");
-        scanf("%s",danh_sach_lop[i].ten_sinhvien);
-        printf("nhap nam sinh sinh vien: ");
-        scanf("%i",&danh_sach_lop[i].namsinh_sinhvien);
-    }
+void nhap_danh_sach_sv(struct SinhVien danh_sach_lop[], int *p_so_luong_sv) {
+  printf("nhap so luong sinh vien: ");
+  scanf("%i", p_so_luong_sv);
+  for (int i = 0; i < *p_so_luong_sv; i++) {
+    printf("nhap ma sinh vien: ");
+    scanf(" %[^\n]s", danh_sach_lop[i].ma_sinhvien);
+
+    printf("nhap ho dem sinh vien: ");
+    scanf(" %[^\n]s", danh_sach_lop[i].hodem_sinhvien);
+
+    printf("nhap ten sinh vien: ");
+    scanf(" %[^\n]s", danh_sach_lop[i].ten_sinhvien);
+
+    printf("nhap nam sinh sinh vien: ");
+    scanf("%d", &danh_sach_lop[i].namsinh_sinhvien);
+  }
 }
