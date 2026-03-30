28e736e4236658528fb88d27478548ec66fa8ed6
cong2812 <voanhcong281207@gmail.com>
2026-03-30 16:45:36 +0700

    chroe: update config

--- nhap_danh_sach_sv.c
+++ nhap_danh_sach_sv.c
@@ -12,5 +12,14 @@ void nhap_danh_sach_sv(struct SinhVien danh_sach_lop[],int *p_so_luong_sv){
         scanf("%s",danh_sach_lop[i].ten_sinhvien);
         printf("nhap nam sinh sinh vien: ");
         scanf("%i",&danh_sach_lop[i].namsinh_sinhvien);
+    };
+    for (int i = 0; i < so_luong_sv - 1; i++) {
+        for (int j = i + 1; j < so_luong_sv; j++) {
+            if (strcmp(danh_sach_lop[i].ma_sinhvien, danh_sach_lop[j].ma_sinhvien) > 0) {
+                struct SinhVien temp = danh_sach_lop[i];
+                danh_sach_lop[i] = danh_sach_lop[j];
+                danh_sach_lop[j] = temp;
+            }
+        }
     }
 }
