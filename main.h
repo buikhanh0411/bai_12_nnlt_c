c6657d31cbafe4551482be0b1ff080f4223b4d5a
khanh <khanhdacodau2007@gmail.com>
2026-03-30 19:47:20 +0700

    feat: reduce char list to 20

--- main.h
+++ main.h
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 
 struct SinhVien {
-  char ma_sinhvien[200];
-  char hodem_sinhvien[200];
-  char ten_sinhvien[200];
+  char ma_sinhvien[20];
+  char hodem_sinhvien[20];
+  char ten_sinhvien[20];
   int namsinh_sinhvien;
 };
 
@@ -15,7 +15,6 @@ void loop_menu(struct SinhVien danh_sach_lop[], int *p_so_luong_sv);
 void nhap_danh_sach_sv(struct SinhVien danh_sach_lop[], int *p_so_luong_sv);
 void xuat_danh_sach_sv(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
 
-void in_sv_nho_tuoi_nhat(struct SinhVien danh_sach_lop[],
-                         int p_so_luong_sv); // cong
+void in_sv_nho_tuoi_nhat(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
 void in_sv_sinh_truoc_nam(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
 void sap_sep_sv_theo_ten(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
