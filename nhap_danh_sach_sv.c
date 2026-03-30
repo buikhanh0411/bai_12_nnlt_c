28fe2c048dbc6fa8923a6a3e7127ba4df47dec30
cong2812 <voanhcong281207@gmail.com>
2026-03-30 19:53:19 +0700

    chore :update config

--- nhap_danh_sach_sv.c
+++ nhap_danh_sach_sv.c
@@ -13,8 +13,8 @@ void nhap_danh_sach_sv(struct SinhVien danh_sach_lop[],int *p_so_luong_sv){
         printf("nhap nam sinh sinh vien: ");
         scanf("%i",&danh_sach_lop[i].namsinh_sinhvien);
     };
-    for (int i = 0; i < so_luong_sv - 1; i++) {
-        for (int j = i + 1; j < so_luong_sv; j++) {
+    for (int i = 0; i < *p_so_luong_sv - 1; i++) {
+        for (int j = i + 1; j < *p_so_luong_sv; j++) {
             if (strcmp(danh_sach_lop[i].ma_sinhvien, danh_sach_lop[j].ma_sinhvien) > 0) {
                 struct SinhVien temp = danh_sach_lop[i];
                 danh_sach_lop[i] = danh_sach_lop[j];
--- xuat_danh_sach_sv.c
+++ xuat_danh_sach_sv.c
@@ -4,7 +4,7 @@ void xuat_danh_sach_sv(struct SinhVien danh_sach_lop[],int p_so_luong_sv){
 
     printf("\nDanh sach sinh vien:\n");
     printf("%-10s %-30s %-20s %-10s\n", "Ma SV", "Ho Dem Sinh Vien", "Ten Sinh Vien", "Nam Sinh");
-    for (int i = 0; i < so_luong_sv; i++) {
+    for (int i = 0; i < *p_so_luong_sv; i++) {
         printf("%-10s %-30s %-20s %-10d\n",
                danh_sach_lop[i].ma_sinhvien,
                danh_sach_lop[i].hodem_sinhvien,
