1dda56a3ec9bc6c3dbad66bfcecedc3f0384ff5f
khanh <khanhdacodau2007@gmail.com>
2026-03-30 20:47:24 +0700

    chore: update output more beautiful

--- xuat_danh_sach_sv.c
+++ xuat_danh_sach_sv.c
@@ -1,15 +1,24 @@
 #include "main.h"
 
-void xuat_danh_sach_sv(struct SinhVien danh_sach_lop[],int p_so_luong_sv){
+void xuat_danh_sach_sv(struct SinhVien danh_sach_lop[], int p_so_luong_sv) {
+    if (p_so_luong_sv == 0) {
+        printf("\n[!] Danh sach dang trong. Hay nhap sinh vien truoc!\n");
+        return;
+    }
 
-    printf("\nDanh sach sinh vien:\n");
-    printf("%-10s %-30s %-20s %-10s\n", "Ma SV", "Ho Dem Sinh Vien", "Ten Sinh Vien", "Nam Sinh");
+    printf("\n                      DANH SACH SINH VIEN\n");
+    printf("+-----+------------+---------------------------+----------------+----------+\n");
+    printf("| STT | Ma SV      | Ho Dem                    | Ten            | Nam Sinh |\n");
+    printf("+-----+------------+---------------------------+----------------+----------+\n");
+
     for (int i = 0; i < p_so_luong_sv; i++) {
-        printf("%-10s %-30s %-20s %-10d\n",
+        printf("| %-3d | %-10s | %-25s | %-14s | %-8d |\n",
+               i + 1,
                danh_sach_lop[i].ma_sinhvien,
                danh_sach_lop[i].hodem_sinhvien,
                danh_sach_lop[i].ten_sinhvien,
                danh_sach_lop[i].namsinh_sinhvien);
     }
+    printf("+-----+------------+---------------------------+----------------+----------+\n");
+    printf("Tong so: %d sinh vien.\n", p_so_luong_sv);
 }
-    
