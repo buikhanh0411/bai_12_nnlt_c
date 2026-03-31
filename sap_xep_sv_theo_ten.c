511b062cce95edbd12aeeb7f7a3e19952107c329
Dang Ba Duy <thgchuacc@gmail.com>
2026-03-31 23:12:59 +0700

    chore:use func in utils.h

--- sap_xep_sv_theo_ten.c
+++ sap_xep_sv_theo_ten.c
@@ -1,30 +1,33 @@
-#include "main.h"
+#include "utils.h"
 
-
-void sap_sep_sv_theo_ten(struct SinhVien danh_sach_lop[], int p_so_luong_sv)
+void sap_sep_sv_theo_ten(struct SinhVien danh_sach_sv[], int p_so_luong_sv)
 {
-    for (int i = 0; i < p_so_luong_sv - 1; i++)
-    {
-        for (int j = 0; j < p_so_luong_sv - i - 1; j++)
-        {
-            if (strcmp(danh_sach_lop[j].ten_sinhvien,
-                       danh_sach_lop[j + 1].ten_sinhvien) > 0)
-            {
-                // hoán đổi toàn bộ struct
-                struct SinhVien temp = danh_sach_lop[j];
-                danh_sach_lop[j] = danh_sach_lop[j + 1];
-                danh_sach_lop[j + 1] = temp;
-            }
-        }
-    }
-}
-for (int i = 0; i < p_so_luong_sv; i++)
-    {
-        printf("%s | %s | %s | %d\n",
-               danh_sach_lop[i].ma_sinhvien,
-               danh_sach_lop[i].hodem_sinhvien,
-               danh_sach_lop[i].ten_sinhvien,
-               danh_sach_lop[i].namsinh_sinhvien);
-    }
-}
 
+  // Copy mang sinh vien (tranh anh huong vao mang chinh)
+  struct SinhVien cp_danh_sach_lop[p_so_luong_sv];
+  for (int i = 0; i < p_so_luong_sv; i++)
+  {
+    copy_sinh_vien(&cp_danh_sach_lop[i], danh_sach_sv[i]);
+  }
+
+  for (int i = 0; i < p_so_luong_sv - 1; i++)
+  {
+    for (int j = 0; j < p_so_luong_sv - i - 1; j++)
+    {
+      if (strcmp(cp_danh_sach_lop[j].ten_sinhvien,
+                 cp_danh_sach_lop[j + 1].ten_sinhvien) > 0)
+      {
+        // hoán đổi toàn bộ struct
+        struct SinhVien temp;
+        copy_sinh_vien(&temp, cp_danh_sach_lop[j]);
+        copy_sinh_vien(&cp_danh_sach_lop[j], cp_danh_sach_lop[j + 1]);
+        copy_sinh_vien(&cp_danh_sach_lop[j + 1], temp);
+      }
+    }
+  }
+
+  for (int i = 0; i < p_so_luong_sv; i++)
+  {
+    in_sinh_vien(cp_danh_sach_lop[i], -1);
+  }
+}
