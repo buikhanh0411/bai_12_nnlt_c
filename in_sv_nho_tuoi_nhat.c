4a3e7dd11a015a532cdf7d55dca82fa0c5e357f2
khanh <khanhdacodau2007@gmail.com>
2026-03-31 20:28:04 +0700

    update: use copy array insead
    fix: fix errror syntax
    add: copy array instead of using main danh_sach_lop array

--- in_sv_nho_tuoi_nhat.c
+++ in_sv_nho_tuoi_nhat.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
-void in_sv_nho_tuoi_nhat(struct SinhVien danh_sach_lop[],int p_so_luong_sv){
-   printf("in so luong sinh vien");
-}
- int max = sv[0].namsinh;
+void in_sv_nho_tuoi_nhat(struct SinhVien danh_sach_lop[], int p_so_luong_sv) {
+  printf("in so luong sinh vien");
 
-    for(int i=1;i<n;i++){
-        if(sv[i].namsinh > max){
-            max = sv[i].namsinh;
-        }
+  // Copy mang sinh vien (tranh anh huong vao mang chinh)
+  struct SinhVien cp_danh_sach_lop[p_so_luong_sv];
+  for (int i = 0; i < p_so_luong_sv; i++) {
+    cp_danh_sach_lop[i] = danh_sach_lop[i];
+  }
+
+  int max = cp_danh_sach_lop[0].namsinh_sinhvien;
+
+  for (int i = 1; i < p_so_luong_sv; i++) {
+    if (cp_danh_sach_lop[i].namsinh_sinhvien > max) {
+      max = cp_danh_sach_lop[i].namsinh_sinhvien;
     }
+  }
 
-    printf("\nSinh vien nho tuoi nhat:\n");
+  printf("\nSinh vien nho tuoi nhat:\n");
 
-    for(int i=0;i<n;i++){
-        if(sv[i].namsinh == max){
-            printf("%s %s %s %d\n",
-            sv[i].masv,
-            sv[i].hodem,
-            sv[i].ten,
-            sv[i].namsinh);
-        }
+  for (int i = 0; i < p_so_luong_sv; i++) {
+    if (cp_danh_sach_lop[i].namsinh_sinhvien == max) {
+      printf("%s %s %s %d\n", cp_danh_sach_lop[i].ma_sinhvien,
+             cp_danh_sach_lop[i].hodem_sinhvien,
+             cp_danh_sach_lop[i].ten_sinhvien,
+             cp_danh_sach_lop[i].namsinh_sinhvien);
     }
+  }
 }
