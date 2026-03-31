d4d60e5e378946ba6e00621c3b7d95f7d6a479ea
khanh <khanhdacodau2007@gmail.com>
2026-04-01 01:27:40 +0700

    refactor: move SinhVien struct definition to main.h and update utils.h accordingly

--- src/core/main.h
+++ src/core/main.h
@@ -3,11 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../utils/utils.h"
 
 // \e[1;1H: dua con tro ve goc tren ben trai
 // \e[2J: xoa toan bo man hinh
 #define CLEAR_SCREEN() printf("\033[H\033[J")
+struct SinhVien
+{
+  char ma_sinhvien[20];
+  char hodem_sinhvien[20];
+  char ten_sinhvien[20];
+  int namsinh_sinhvien;
+};
 
 void loop_menu(struct SinhVien danh_sach_sv[], int *p_so_luong_sv);
 void nhap_danh_sach_sv(struct SinhVien danh_sach_sv[], int *p_so_luong_sv);
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,15 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <stdio.h>
 
-struct SinhVien
-{
-  char ma_sinhvien[20];
-  char hodem_sinhvien[20];
-  char ten_sinhvien[20];
-  int namsinh_sinhvien;
-};
+#include "../core/main.h"
 
 void in_danh_sach_sv(struct SinhVien danh_sach[], int so_luong);
 void copy_sinh_vien(struct SinhVien *dest, struct SinhVien src);
