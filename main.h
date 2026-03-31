48a28fdde7bfc921d10b951b07e5b09f5dbd9ebf
khanh <khanhdacodau2007@gmail.com>
2026-03-31 20:26:53 +0700

    feat: remove so_luong arg

--- main.h
+++ main.h
@@ -20,5 +20,5 @@ void loop_menu(struct SinhVien danh_sach_lop[], int *p_so_luong_sv);
 void nhap_danh_sach_sv(struct SinhVien danh_sach_lop[], int *p_so_luong_sv);
 void xuat_danh_sach_sv(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
 void in_sv_nho_tuoi_nhat(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
-void in_sv_sinh_truoc_nam(struct SinhVien danh_sach_lop[], int p_so_luong_sv, int so_luong_sv);
+void in_sv_sinh_truoc_nam(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
 void sap_sep_sv_theo_ten(struct SinhVien danh_sach_lop[], int p_so_luong_sv);
