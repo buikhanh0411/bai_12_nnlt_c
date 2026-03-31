62f100c06ff67d088a8a3f3c627eb545a79312c5
khanh <khanhdacodau2007@gmail.com>
2026-04-01 01:14:38 +0700

    refactor: enhance input validation for student details in nhap_danh_sach_sv function

--- nhap_danh_sach_sv.c
+++ nhap_danh_sach_sv.c
@@ -8,25 +8,101 @@ void nhap_danh_sach_sv(struct SinhVien danh_sach_sv[], int *p_so_luong_sv)
   printf("==========================================\n");
   printf("     NHAP DANH SACH SINH VIEN MOI\n");
   printf("==========================================\n");
-  printf("Nhap so luong sinh vien can them: ");
-  scanf("%d", &n);
+
+  do
+  {
+    printf("Nhap so luong sinh vien can them: ");
+    if (scanf("%d", &n) != 1)
+    {
+      scanf("%*s");
+      printf("[!] So luong phai la mot so nguyen duong, vui long nhap lai.\n");
+      n = -1;
+      continue;
+    }
+
+    if (n <= 0 || n > 50 - *p_so_luong_sv)
+    {
+      printf("[!] So luong khong hop le. (0 < n <= %d), vui long nhap lai.\n", 50 - *p_so_luong_sv);
+      n = -1;
+    }
+  } while (n <= 0);
 
   for (int i = 0; i < n; i++)
   {
     printf("\n[ Sinh vien thu %d ]\n", (*p_so_luong_sv) + 1);
 
-    printf(" + Ma sinh vien: ");
-    scanf("%s", danh_sach_sv[*p_so_luong_sv].ma_sinhvien);
+    do
+    {
+      printf(" + Ma sinh vien: ");
+      if (scanf("%19s", danh_sach_sv[*p_so_luong_sv].ma_sinhvien) != 1)
+      {
+        scanf("%*s");
+        printf("[!] Ma sinh vien khong duoc de trong. Nhap lai.\n");
+      }
+      else if (strlen(danh_sach_sv[*p_so_luong_sv].ma_sinhvien) == 0)
+      {
+        printf("[!] Ma sinh vien khong duoc de trong. Nhap lai.\n");
+      }
+      else
+      {
+        break;
+      }
+    } while (1);
 
-    printf(" + Ho va ten dem: ");
-    // Dung " %[^ \n]" de bo qua ky tu xuong dong va nhap ca chuoi
-    scanf(" %[^\n]", danh_sach_sv[*p_so_luong_sv].hodem_sinhvien);
+    do
+    {
+      printf(" + Ho va ten dem: ");
+      if (scanf(" %19[^\n]", danh_sach_sv[*p_so_luong_sv].hodem_sinhvien) != 1)
+      {
+        scanf("%*s");
+        printf("[!] Ho dem khong hop le. Nhap lai.\n");
+      }
+      else if (strlen(danh_sach_sv[*p_so_luong_sv].hodem_sinhvien) == 0)
+      {
+        printf("[!] Ho dem khong duoc de trong. Nhap lai.\n");
+      }
+      else
+      {
+        break;
+      }
+    } while (1);
 
-    printf(" + Ten: ");
-    scanf("%s", danh_sach_sv[*p_so_luong_sv].ten_sinhvien);
+    do
+    {
+      printf(" + Ten: ");
+      if (scanf("%19s", danh_sach_sv[*p_so_luong_sv].ten_sinhvien) != 1)
+      {
+        scanf("%*s");
+        printf("[!] Ten khong hop le. Nhap lai.\n");
+      }
+      else if (strlen(danh_sach_sv[*p_so_luong_sv].ten_sinhvien) == 0)
+      {
+        printf("[!] Ten khong duoc de trong. Nhap lai.\n");
+      }
+      else
+      {
+        break;
+      }
+    } while (1);
 
-    printf(" + Nam sinh: ");
-    scanf("%d", &danh_sach_sv[*p_so_luong_sv].namsinh_sinhvien);
+    int year;
+    do
+    {
+      printf(" + Nam sinh: ");
+      if (scanf("%d", &year) != 1)
+      {
+        scanf("%*s");
+        printf("[!] Nam sinh phai la so nguyen. Nhap lai.\n");
+        continue;
+      }
+      if (year < 1900 || year > 2026)
+      {
+        printf("[!] Nam sinh phai duoi 2026. Nhap lai.\n");
+        continue;
+      }
+      danh_sach_sv[*p_so_luong_sv].namsinh_sinhvien = year;
+      break;
+    } while (1);
 
     (*p_so_luong_sv)++;
   }
