4a35347efc4860a69fa9013b3c19dc7c12175c6e
cong2812 <voanhcong281207@gmail.com>
2026-04-07 16:04:58 +0700

    #

--- src/core/xoa_danh_sach_sinh_vien.c
+++ src/core/xoa_danh_sach_sinh_vien.c
@@ -9,5 +9,5 @@ void xoa_toan_bo_danh_sach_sinh_vien(struct SinhVien danh_sach_sv,int *p_so_luon
         memset(danh_sach_sv[i].ten_sinhvien, 0, sizeof(danh_sach_sv[i].ten_sinhvien));
         danh_sach_sv[i].namsinh_sinhvien = 0;
     };
-    printf("Da xoa toan bo danh sach sinh vien!\n")
+    printf("Da xoa toan bo danh sach sinh vien!\n");
 }
