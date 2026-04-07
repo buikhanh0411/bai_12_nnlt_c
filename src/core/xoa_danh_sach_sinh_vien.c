a7be29e86dc3b61cdc8e54cf7d94869db6a93061
khanh <khanhdacodau2007@gmail.com>
2026-04-07 16:03:57 +0700

    feat: change to poiter

--- src/core/xoa_danh_sach_sinh_vien.c
+++ src/core/xoa_danh_sach_sinh_vien.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void xoa_toan_bo_danh_sach_sinh_vien(struct SinhVien danh_sach_sv,int *p_so_luong_sv){
+void xoa_toan_bo_danh_sach_sinh_vien(struct SinhVien *danh_sach_sv,int *p_so_luong_sv){
     *p_so_luong_sv = 0;
     for (int i = 0; i < 50; i++)
     {
@@ -9,5 +9,5 @@ void xoa_toan_bo_danh_sach_sinh_vien(struct SinhVien danh_sach_sv,int *p_so_luon
         memset(danh_sach_sv[i].ten_sinhvien, 0, sizeof(danh_sach_sv[i].ten_sinhvien));
         danh_sach_sv[i].namsinh_sinhvien = 0;
     };
-    printf("Da xoa toan bo danh sach sinh vien!\n")
+    printf("Da xoa toan bo danh sach sinh vien!\n");
 }
